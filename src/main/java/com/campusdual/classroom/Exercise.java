06ca084dd1fe6bbfe02e672692b2ba91841bdee9
Alvaro Sainz <alvaroalonsovp@outlook.es>
2025-10-28 18:39:49 +0100

    Exercise 12 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,7 +2,33 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
+        Car C = new Car();
+        System.out.println("Tacómetro cero? " + C.isTachometerEqualToZero());
+
+        C.start();
+        C.start();
+
+
+        C.stop();
+        C.accelerate();
+        C.stop();
+
+        for (int i = 0; i < 15; i++) {
+            C.accelerate();
+        }
+
+        for (int i = 0; i < 20; i++) {
+            C.brake();
+        }
+
+        C.turnAngleOfWheels(20);
+        C.turnAngleOfWheels(50);
+
+        C.setReverse(true);
+        C.accelerate();
+        C.setReverse(true);
+
+        C.showDetails();
 
     }
-
 }
